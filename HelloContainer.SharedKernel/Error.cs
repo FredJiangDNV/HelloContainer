f6f14e680b50acc35e5eac543dd15e3673c5cd8a
Fred Jiang <fei.fred.jiang@dnv.com>
2025-07-27 20:29:28 +0800

    Add result object

---
+++ HelloContainer.SharedKernel/Error.cs
@@ -0,0 +1,22 @@
+namespace HelloContainer.SharedKernel
+{
+    public record Error(string Code, string Message)
+    {
+        public static readonly Error None = new(string.Empty, string.Empty);
+
+        public static Error NotFound(string code, string message) =>
+            new($"NotFound.{code}", message);
+
+        public static Error Validation(string code, string message) =>
+            new($"Validation.{code}", message);
+
+        public static Error Unauthorized(string code, string message) =>
+            new($"Unauthorized.{code}", message);
+
+        public static Error Forbidden(string code, string message) =>
+            new($"Forbidden.{code}", message);
+
+        public static Error Internal(string code, string message) =>
+            new($"Internal.{code}", message);
+    }
+} 
